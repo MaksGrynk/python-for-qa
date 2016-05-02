a993c56432809dd6ac8e2f5bc0c32ca1e5d9e4be
MaksGrynk <maksgrinkiv@gmail.com>
2016-05-03 00:02:20 +0300

    Update . Done 4 and 5

--- lesson2-1/Level2.py
+++ lesson2-1/Level2.py
@@ -1,6 +1,18 @@
-string1 = " hi hi bi hihih"
-print string1.count("hi")
+# Write a program to calculate the number of times that the string 'hi' appears anywhere in the given string and change 'hi' to 'bye'. 
+# Case should be ignored.
 
-string = " dev hi  by hi"
-print string
-print string.replace('hi', 'bye', 1)
+string = " hi hi bi hihih"
+
+print "original string : ", string
+print "count Hi : ", string.count("hi")
+print "replace Hi with by : ", string.replace('hi', 'bye', 1)
+
+# Unite three lists to one and print it in reverse order
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+list3 = [7, 8, 9]
+
+List = list1+list2+list3
+print "List : ", List
+List.reverse();
+print "Reverse List : ", List
