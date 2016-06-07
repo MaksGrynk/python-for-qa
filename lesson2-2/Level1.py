10007661e3b102fb5acbed2e5bacf0853b75c04b
MaksGrynk <maksgrinkiv@gmail.com>
2016-06-08 01:17:13 +0300

    done

--- lesson2-2/Level1.py
+++ lesson2-2/Level1.py
@@ -1,20 +1,9 @@
-'''
-Write a function which takes a string and returns its first 10 words concatenated with the last 10 characters.
-
-Write a function that takes a comma-separated string and returns the last element (separated by the last comma) or the entire string, if there is no comma in it.
-
-Write a function that takes a list and returns a new list that contains all the elements of the first list minus all the duplicates:
-
-[1, 2, 3, 4, 4, 6, 2] -> [1, 3, 6]
-
-'''
-##############################################
 # Write a function which takes a string and returns its first 10 words concatenated with the last 10 characters.
 def lines():
     str_end = strin[-1::-1]
     print strin[0:10]+str_end[0:10]
 lines()
-#################################################
+#----------------------------------------
 # Write a function that takes a list and returns a new list that contains all the elements of the first list
 # minus all the duplicates: [1, 2, 3, 4, 4, 6, 2] -> [1, 3, 6]
 
@@ -28,7 +17,7 @@ def rem_dublic():
             output.remove(i)
         print output
 rem_dublic()
-#################################################
+#-----------------------------------------
 Write a function that takes a comma-separated string and returns the last element (separated by the last comma) or the entire string, if there is no comma in it.
 
 def endstring():
@@ -42,7 +31,7 @@ def endstring():
 endstring()
 
 
-#################################################
+#---------------------------------------
 
 # a function which takes a string and returns all words concatenated with the last 7 characters
 def lens():
